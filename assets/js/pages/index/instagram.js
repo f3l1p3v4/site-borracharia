7d8d77690dc923c40d7bad0594c260ab41e7e47e
f3l1p3v4 <fvaldez018@gmail.com>
2024-10-08 23:35:04 -0400

    added tag trafego

--- assets/js/pages/index/instagram.js
+++ assets/js/pages/index/instagram.js
@@ -1,27 +1,30 @@
-const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+document.addEventListener('DOMContentLoaded', function () {
+  const locationButtons = document.querySelectorAll('.sendLocationBtn');
 
-locationButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        const whatsappNumber = '18996453979'; // Substitua pelo número correto
-        const message = `Olá, preciso de socorro. Minha localização: https://www.google.com/maps?q=${latitude},${longitude}`;
-        const whatsappUrl = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(message)}`;
+  locationButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          const whatsappNumber = '18996453979'; // Substitua pelo número correto
+          const message = `Olá, preciso de socorro. Minha localização: https://www.google.com/maps?q=${latitude},${longitude}`;
 
-        // Diferenciar comportamento para iOS e Android
-        if (isIOS) {
-          window.location.href = whatsappUrl; // Redireciona diretamente no iOS
-        } else {
-          window.open(whatsappUrl, '_blank'); // Abre nova aba no Android
-        }
+          // Diferenciar entre iOS e Android
+          const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+          const whatsappUrl = isIOS 
+            ? `whatsapp://send?phone=${whatsappNumber}&text=${encodeURIComponent(message)}` 
+            : `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(message)}`;
 
-      }, function(error) {
-        alert('Não foi possível obter sua localização. Verifique se as permissões estão ativadas.');
-      });
-    } else {
-      alert('Geolocalização não é suportada pelo seu navegador.');
-    }
+          // Redireciona o usuário
+          window.location.href = whatsappUrl;
+
+        }, function(error) {
+          alert('Não foi possível obter sua localização. Verifique se as permissões estão ativadas.');
+        });
+      } else {
+        alert('Geolocalização não é suportada pelo seu navegador.');
+      }
+    });
   });
 });
